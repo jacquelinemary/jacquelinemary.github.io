3f6fbeecf45a250bc460f2ff60b4afe612013f27
Enoch Riese <enoch.riese@gmail.com>
2014-03-23 22:50:54 -0400

    unknown changes

--- js/videos.js
+++ js/videos.js
@@ -5,10 +5,4 @@ $(".vid").on("click", function(e) {
   else {
     $(this).addClass("clicked")
   }
-  // $(this).css({"display": "block", "height": $(this).children().height(), "width": $(this).children().width(), "border-radius": "0px", "padding": "30px"});
-  // $(this).children().css({"display": "block"});
 });
-// $(".vid").on("dblclick", function(e) {
-//   $(this).css({"display": "inline-block", "height": "200px", "width": "200px", "border-radius": "100px", "padding": 0})
-//   $(this).children().css("display", "none");
-// });
