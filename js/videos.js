90e5006fb52e929005c8cd189182d38e0ad10aa5
Enoch Riese <enoch.riese@gmail.com>
2014-03-24 02:30:29 -0400

    videos now reload

--- js/videos.js
+++ js/videos.js
@@ -1,5 +1,7 @@
 $(".vid").on("click", function(e) {
   $(this).toggleClass("clicked")
   $(".vid").toggleClass("hidden")
-  $(this).children().first().toggleClass("hidden")
+  $(this).children(".title").toggleClass("hidden")
+  src = $(this).children("iframe").attr("src")
+  $(this).children("iframe").attr("src", src)
 });
